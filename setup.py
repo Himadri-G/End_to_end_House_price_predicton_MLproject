5bd06de5f1f8d2f4d7ea6dd79b9f10d17d4a6aa1
Himadri-G <himadrigoswami191118@gmail.com>
2026-01-28 19:07:45 +0530

    Add initial package metadata and setup configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "House_price_prediction",
+    version="0.0.0",
+    author="Himadri",
+    author_email="himadrigoswami191118@gmail.com",
+    packages=find_packages()
+)
